d1beec3b82ede1970bf4ef249a388db8a5f8412c
wikili202 <wikili202@gmail.com>
2022-05-12 14:37:13 +0200

    Update server.lua

--- wk_hudid/server/server.lua
+++ wk_hudid/server/server.lua
@@ -1,6 +1,11 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+--[[
+Script created by wikili202 (https://github.com/wikili202)
+wikili202#5601
+--]]
+
 
 ESX.RegisterServerCallback("wkhudid:set", function(source, cb)
     local source = source
