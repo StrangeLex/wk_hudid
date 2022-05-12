bb90034e1b331bc6464640e81cdc744097d8a6c6
wikili202 <wikili202@gmail.com>
2022-05-12 14:31:58 +0200

    Update config.lua

--- wk_hudid/config.lua
+++ wk_hudid/config.lua
@@ -8,4 +8,4 @@ wikili202 = {
 
 -- Style 1 = (left = 5, top = 5, bordercolor = "yellow")
 -- Style 2 = (left = 3.8, top = 79)
--- Style 3 = (left = 30, top = 90)
+-- Style 3 = (left = 30, top = 90, circle = "#931A1A")
