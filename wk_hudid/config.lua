d256329bb77afddd597f37db55e1c2bc100e5acb
wikili202 <wikili202@gmail.com>
2022-05-12 14:31:34 +0200

    Update config.lua

--- wk_hudid/config.lua
+++ wk_hudid/config.lua
@@ -1,11 +1,11 @@
 wikili202 = {
-    Style = 3,
-    left = "30",
-    top = "90",
+    Style = 1,
+    left = "5",
+    top = "5",
     bordercolor = "yellow", -- Style 2 Only (https://htmlcolorcodes.com/)
     circlecolor = "#931A1A", -- Style 3 Only (https://htmlcolorcodes.com/)
 }
 
 -- Style 1 = (left = 5, top = 5, bordercolor = "yellow")
 -- Style 2 = (left = 3.8, top = 79)
--- Style 3
+-- Style 3 = (left = 30, top = 90)
